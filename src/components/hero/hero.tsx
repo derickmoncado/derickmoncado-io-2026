96e497db0ad956a7628f453b6b1d3bb718d234cc
Derick Moncado <derickmoncado@gmail.com>
2026-01-21 14:55:43 -0800

    Styled collab status thing

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -6,17 +6,20 @@ import Link from "next/link";
 export default function Hero() {
 	return (
 		<section className={styles["hero"]}>
-			<div className="container">
-				<div className={styles["hero__content"]}>
-					<div className={styles["hero__content__details"]}>
-						<div className={styles["hero__content__details__collab"]}></div>
-						<div className={styles["hero__content__details__text"]}></div>
-						<div className={styles["hero__content__details__ctas"]}></div>
+			<div className={styles["hero__content"]}>
+				<div className={styles["hero__content__details"]}>
+					<div className={styles["hero__content__details__collab"]}>
+						<p className={styles.status}>
+							<span></span>Open to Collaborations
+						</p>
+						<p>As of February 2026</p>
 					</div>
+					<div className={styles["hero__content__details__text"]}></div>
+					<div className={styles["hero__content__details__ctas"]}></div>
+				</div>
 
-					<div className={styles["hero__content__image"]}>
-						<div className={styles["hero__content__image__frame"]}></div>
-					</div>
+				<div className={styles["hero__content__image"]}>
+					<div className={styles["hero__content__image__frame"]}></div>
 				</div>
 			</div>
 		</section>

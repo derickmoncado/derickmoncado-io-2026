aeb05857ec5d39713b4fc23fca47d774b0fa724f
Derick Moncado <derickmoncado@gmail.com>
2026-01-22 14:51:35 -0800

    Styling the hero

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ArrowUpRight, Mail } from "lucide-react";
 import styles from "./hero.module.scss";
 import Link from "next/link";
 
@@ -14,8 +15,25 @@ export default function Hero() {
 						</p>
 						<p>As of February 2026</p>
 					</div>
-					<div className={styles["hero__content__details__text"]}></div>
-					<div className={styles["hero__content__details__ctas"]}></div>
+					<div className={styles["hero__content__details__text"]}>
+						<h1>Hey, I’m Derick</h1>
+						<h2>Developer, Designer, and Content Creator</h2>
+						<p>
+							I’m based in Southern California and have over 12 years
+							<br />
+							experience in design, web, and application development
+						</p>
+					</div>
+					<div className={styles["hero__content__details__ctas"]}>
+						<Link href="https://www.youtube.com/@derickmoncado">
+							Watch Me
+							<ArrowUpRight />
+						</Link>
+						<Link href="">
+							Contact Me
+							<Mail />
+						</Link>
+					</div>
 				</div>
 
 				<div className={styles["hero__content__image"]}>

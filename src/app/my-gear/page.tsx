2fd1ef8387b6ca31cfa6f7ff5eb8bae7b5fad4b1
Derick Moncado <derickmoncado@gmail.com>
2026-01-29 18:14:32 -0800

    Built out rest of links

--- src/app/my-gear/page.tsx
+++ src/app/my-gear/page.tsx
@@ -27,6 +27,42 @@ export default function MyGear() {
 							</div>
 						</div>
 					</Link>
+
+					<Link className={styles["link"]} href="#">
+						<div className={styles["link__inner"]}>
+							<div className={styles["link__inner__image"]}></div>
+							<div className={styles["link__inner__details"]}>
+								<h2>Camera Gear</h2>
+								<p>Everything I use to shoot and edit videos</p>
+								<span className={styles["link__inner__details__last-updated"]}>Updated Jul 23rd 2026</span>
+								<ArrowRight />
+							</div>
+						</div>
+					</Link>
+
+					<Link className={styles["link"]} href="#">
+						<div className={styles["link__inner"]}>
+							<div className={styles["link__inner__image"]}></div>
+							<div className={styles["link__inner__details"]}>
+								<h2>Development Stack</h2>
+								<p>All apps, tools, and services I use to build stuff</p>
+								<span className={styles["link__inner__details__last-updated"]}>Updated Jul 23rd 2026</span>
+								<ArrowRight />
+							</div>
+						</div>
+					</Link>
+
+					<Link className={styles["link"]} href="#">
+						<div className={styles["link__inner"]}>
+							<div className={styles["link__inner__image"]}></div>
+							<div className={styles["link__inner__details"]}>
+								<h2>Another Item</h2>
+								<p>Subheading goes here</p>
+								<span className={styles["link__inner__details__last-updated"]}>Updated Jul 23rd 2026</span>
+								<ArrowRight />
+							</div>
+						</div>
+					</Link>
 				</div>
 			</div>
 		</section>

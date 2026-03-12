da1ab95559197d23a50f1f1a0f963c3a5ea35b00
Derick Moncado <Derick.moncado@activision.com>
2026-03-11 18:17:01 -0700

    Started on lInks page build/styles

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -5,15 +5,24 @@ import styles from "./page.module.scss";
 
 export default function LinksPage() {
 	return (
-		<section className={styles["links"]}>
+		<main className={styles["links"]}>
 			<div className={styles["links__content"]}>
-				<div className={styles["links__content__heading"]}>
-					<h2>Links</h2>
-					<p>photo and stuff here</p>
+				<div className={styles["links__content__hero"]}>
+					<div className={styles["links__content__hero__image"]}>
+						<div className={styles["links__content__hero__image__frame"]}>
+							<video className={styles["links__content__hero__image__video"]} autoPlay muted loop playsInline preload="auto" aria-hidden="true" tabIndex={-1} disablePictureInPicture>
+								<source src="/videos/hero-clip.mp4" />
+							</video>
+						</div>
+					</div>
+					<div className={styles["links__content__hero__headline"]}>
+						<h2>@derickmoncado</h2>
+						<p>Developer, Designer, and Content Creator</p>
+					</div>
 				</div>
 
 				<div className={styles["links__content__body"]}>body here</div>
 			</div>
-		</section>
+		</main>
 	);
 }

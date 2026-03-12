a8b60aee79b28b7a58b78b822e58911cc3c39785
Derick Moncado <Derick.moncado@activision.com>
2026-03-12 10:37:01 -0700

    Social icons styled yahoooo

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import styles from "./page.module.scss";
 
 export default function LinksPage() {
@@ -20,20 +21,24 @@ export default function LinksPage() {
 					</div>
 				</div>
 
-				<div className={styles["links__content__links"]}>
-					<a href="https://www.youtube.com/@derickmoncado" className={styles["links__content__links__youtube"]} target="_blank" rel="noopener noreferrer">
-						Subscribe
-					</a>
-					<a href="https://www.threads.com/@derickmoncado.jpg" className={styles["links__content__links__threads"]} target="_blank" rel="noopener noreferrer">
-						Follow
-					</a>
-					<a href="https://www.instagram.com/derickmoncado.jpg/" className={styles["links__content__links__instagram"]} target="_blank" rel="noopener noreferrer">
-						Follow
-					</a>
-					<a href="https://www.linkedin.com/in/derickmoncado/" className={styles["links__content__links__linkedin"]} target="_blank" rel="noopener noreferrer">
-						LinkedIn
-					</a>
-				</div>
+					<div className={styles["links__content__links"]}>
+						<a href="https://www.youtube.com/@derickmoncado" className={styles["links__content__links__youtube"]} target="_blank" rel="noopener noreferrer">
+							<Image src="/images/youtube.svg" alt="YouTube icon" width={18} height={18} />
+							Subscribe
+						</a>
+						<a href="https://www.threads.com/@derickmoncado.jpg" className={styles["links__content__links__threads"]} target="_blank" rel="noopener noreferrer">
+							<Image src="/images/threads.svg" alt="Threads icon" width={18} height={18} />
+							Follow
+						</a>
+						<a href="https://www.instagram.com/derickmoncado.jpg/" className={styles["links__content__links__instagram"]} target="_blank" rel="noopener noreferrer">
+							<Image src="/images/instagram.svg" alt="Instagram icon" width={18} height={18} />
+							Follow
+						</a>
+						<a href="https://www.linkedin.com/in/derickmoncado/" className={styles["links__content__links__linkedin"]} target="_blank" rel="noopener noreferrer">
+							<Image src="/images/linkedin.svg" alt="LinkedIn icon" width={18} height={18} />
+							LinkedIn
+						</a>
+					</div>
 			</div>
 		</main>
 	);

23a87ecba1f55adf587fe5490c9b132fed5f9aae
Derick Moncado <derickmoncado@gmail.com>
2026-08-04 10:38:46 -0700

    Made LLC page

--- src/app/moncadomedia/page.tsx
+++ src/app/moncadomedia/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { ChevronLeft } from "lucide-react";
 import styles from "./page.module.scss";
@@ -12,7 +13,12 @@ export default function MoncadoMediaPage() {
 					</Link>
 				</div>
 
-				<h1>moncadomedia</h1>
+				<Image className={styles["moncadomedia__content__logo"]} src="/images/moncadomedia-logo.png" alt="Moncado Media" width={600} height={153} priority />
+				<p>
+					MoncadoMedia LLC owns and operates a small collection of internet businesses and ventures created by <Link href="/">Derick Moncado</Link>, spanning YouTube, eCommerce, and affiliate marketing. <br />
+					<br />
+					For partnerships, collabs, or inquiries, reach out at <a href="mailto:hello@derickmoncado.io">hello@derickmoncado.io</a>.
+				</p>
 			</div>
 		</main>
 	);
